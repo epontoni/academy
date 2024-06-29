fbc2f5482b9ac56a8d6ab79d6b70b59e1d462088
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2024-06-29 18:12:21 -0300

    add courseId in LessonForm params

--- app/(home)/dashboard/create/lesson/[unitId]/page.tsx
+++ app/(home)/dashboard/create/lesson/[unitId]/page.tsx
@@ -1,15 +1,19 @@
 import { LessonForm } from "@/components/lesson-form";
-import ResourceForm from "@/components/resource-form";
+// import ResourceForm from "@/components/resource-form";
+import { getUnitById } from "@/lib/database/actions/unit.actions";
 
-export default function CreateLessonPage({
+export default async function CreateLessonPage({
   params,
 }: {
   params: { unitId: string };
 }) {
+  const unit = await getUnitById(params.unitId);
+  const courseId = unit.courseId;
+
   return (
     <div>
       Create Lesson Page for unitId: {params.unitId}
-      <LessonForm type="Create" unitId={params.unitId} />
+      <LessonForm type="Create" courseId={courseId} unitId={params.unitId} />
     </div>
   );
 }
